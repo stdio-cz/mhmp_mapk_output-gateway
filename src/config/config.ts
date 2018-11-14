b5308b6c326e5b0d89541393fe32e21b7123a549
Vycpálek Jiří <OP1003@oict.cz>
2018-11-14 17:53:13 +0100

    changed env var to MONGO_CONN, removed app version from env vars

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 module.exports = {
-    app_version: process.env.APP_VERSION,
-    mongo_connection: process.env.MONGO_CONNECTION,
+    app_version: 0.1,
+    mongo_connection: process.env.MONGO_CONN,
     node_env: process.env.NODE_ENV,
     port: process.env.PORT,
 };

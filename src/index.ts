d7fe44a46bf20ad04cecf7599d491129d98bcdb5
Lenka Cackova <lenka.cacko@gmail.com>
2021-08-17 11:38:58 +0000

    feat: telemetry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+// load telemetry before all deps
+import { initTraceProvider } from "@golemio/core/dist/telemetry";
+initTraceProvider("output-gateway");
+
+// start app
 import * as Sentry from "@sentry/node";
 import App from "./App";
 try {
